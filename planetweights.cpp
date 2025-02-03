136850db005f1a2c5464857e620cb79de28b98a4
Ryan Nalbach <103783337+Ryannalbach@users.noreply.github.com>
2025-02-02 20:40:15 -0800

    set up program for writing

---
+++ planetweights.cpp
@@ -0,0 +1,24 @@
+
+/*******************************************************************************
+* Program Name: Planet Weight Calcuator
+* Created Date: 2/2/2025
+* Created By: Ryan Nalbach
+* Purpose: TO calculate your weight on other planets in our solar system
+* Acknowledgements: Tony Gaddis C++ pearson book, professor Cayton Polen
+*******************************************************************************/
+#include <iostream> //to print to console and have input
+#include <iomanip> //to change configuration of output
+using namespace std;
+
+Function Header Block Comment
+/*******************************************************************************
+* Function Name: main()
+* Parameters: None
+* Return Value: int
+* Purpose: to multiple weight input and output answer
+*******************************************************************************/
+
+int main ()
+{
+    
+}
